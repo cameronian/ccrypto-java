a48a96c92ec63dd9adc2a26b724a6e4ad5c6bd37
Ian <cameronian0@protonmail.com>
2022-06-23 20:40:13 +0800

    Fix PBKDF2 hardcoded digest

--- lib/ccrypto/java/engines/pbkdf2_engine.rb
+++ lib/ccrypto/java/engines/pbkdf2_engine.rb
@@ -19,32 +19,10 @@ module Ccrypto
       end
 
       def derive(input, output = :binary)
+        
         begin
 
-          case @config.digest
-          when :sha1
-            dig = org.bouncycastle.crypto.digests.SHA1Digest.new
-          when :sha224
-            dig = org.bouncycastle.crypto.digests.SHA224Digest.new
-          when :sha256
-            dig = org.bouncycastle.crypto.digests.SHA256Digest.new
-          when :sha384
-            dig = org.bouncycastle.crypto.digests.SHA384Digest.new
-          when :sha512
-            dig = org.bouncycastle.crypto.digests.SHA512Digest.new
-          when :sha3_224
-            dig = org.bouncycastle.crypto.digests.SHA3Digest.new(224)
-          when :sha3_256
-            dig = org.bouncycastle.crypto.digests.SHA3Digest.new(256)
-          when :sha3_384
-            dig = org.bouncycastle.crypto.digests.SHA3Digest.new(384)
-          when :sha3_512
-            dig = org.bouncycastle.crypto.digests.SHA3Digest.new(512)
-          else
-            raise KDFEngineException, "Digest #{@config.digest} not supported"
-          end
-
-          skf = javax.crypto.SecretKeyFactory.getInstance("PBKDF2WithHMACSHA256",JCEProvider::DEFProv)
+          skf = javax.crypto.SecretKeyFactory.getInstance("PBKDF2WithHMAC#{@config.digest.upcase}",JCEProvider::DEFProv)
           keySpec = javax.crypto.spec.PBEKeySpec.new(input.to_java.toCharArray,@config.salt, @config.iter, @config.outBitLength)
 
           sk = skf.generateSecret(keySpec)
