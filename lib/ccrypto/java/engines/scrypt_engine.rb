e1b54cbff9dcc1cc9455033683b378fe10c7c4db
Ian <cameronian0@protonmail.com>
2022-09-05 20:25:09 +0800

    Fix mixed up Ruby API with Java API

--- lib/ccrypto/java/engines/scrypt_engine.rb
+++ lib/ccrypto/java/engines/scrypt_engine.rb
@@ -8,13 +8,14 @@ module Ccrypto
       include TR::CondUtils
 
       def initialize(conf, &block)
+
         raise KDFEngineException, "KDF config is expected" if not conf.is_a?(Ccrypto::KDFConfig)
         raise KDFEngineException, "Output bit length (outBitLength) value is not given or not a positive value (#{conf.outBitLength})" if is_empty?(conf.outBitLength) or conf.outBitLength <= 0
         @config = conf
 
         if is_empty?(@config.salt)
           @config.salt = Java::byte[16].new
-          java.security.SecureRandom.getInstance("NativePRNG").random_bytes(@config.salt)
+          java.security.SecureRandom.getInstance("NativePRNG").next_bytes(@config.salt)
         end
       end
 
